c111e19aee7d013be7ff383b25f5822a2b49fa22
procodingclass <88050205+procodingclass@users.noreply.github.com>
2023-11-21 12:51:19 +0530

    Add files via upload

---
+++ SA3/server.py
@@ -0,0 +1,20 @@
+import socket
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+ip_address = '127.0.0.1'
+port = 8000
+
+server.bind((ip_address, port))
+server.listen()
+
+print("Server has started...")
+
+while True:
+    conn, addr = server.accept()
+    
+    # Print Client connected
+    print("Client connected")
+    
+    conn.send("Thank you for connecting".encode())
+    conn.close()
